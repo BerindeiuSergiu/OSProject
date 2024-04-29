652eaa4d3f207936d17fbc1939cbd6212a934862
beribos2 <sergiu.a.berindeiu@gmail.com>
2024-04-29 19:18:32 +0300

    qol

--- izolare.sh
+++ izolare.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 chmod 777 "$1";
 
-echo "Numar caractere : $(echo "$1" | wc -w) ";
+# shellcheck disable=SC2000
+echo "Numar caractere : $(echo "$1" | wc -m) ";
 echo "Numar linii : $(echo "$1" | wc -l) ";
 echo "Numar cuvinte : $(echo "$1" | wc -w) ";
 
@@ -12,7 +13,7 @@ if grep -qE "(dangerous|malicious|risky)" "$1";
         exit 1;
     fi
 
-    if grep -qE "('\x80' - '\xFF')" "$1";
+if grep -qE "['\x80' - '\xFF']" "$1";
     then
         mv "$1" "$2";
         exit 1;
